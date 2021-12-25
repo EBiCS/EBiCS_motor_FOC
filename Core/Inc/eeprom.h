4dda9fb4e2b9506d704d0538fdd997224ee2bf97
stancecoke <hochsitzcola@web.de>
2021-12-25 10:55:59 +0100

    solved confilct between EEPROM and Sysinfo page

--- Core/Inc/eeprom.h
+++ Core/Inc/eeprom.h
@@ -160,7 +160,7 @@
 #define PAGE_SIZE               (uint32_t)FLASH_PAGE_SIZE  /* Page size */
 
 /* EEPROM start address in Flash */
-#define EEPROM_START_ADDRESS  ((uint32_t)ADDR_FLASH_PAGE_62) /* EEPROM emulation start address */
+#define EEPROM_START_ADDRESS  ((uint32_t)ADDR_FLASH_PAGE_79) /* EEPROM emulation start address */
 
 /* Pages 0 and 1 base and end addresses */
 #define PAGE0_BASE_ADDRESS    ((uint32_t)(EEPROM_START_ADDRESS + 0x0000))
